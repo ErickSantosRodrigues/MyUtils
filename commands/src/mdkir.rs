ed4da243fe48f6300c8c9321adcc9c1736b762a4
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-12-20 00:01:42 -0300

    implementation of mkdir

---
+++ commands/src/mdkir.rs
@@ -0,0 +1,9 @@
+use std::fs 
+
+pub fn mkdir(args: Vec<String>) -> Result<(), &'static str> {
+    if args.len() != 2 {
+        return Err("mkdir: missing operand")
+    }
+    
+    fs::create_dir(args[1]).map_err(|_| "mkdir: failed to create directory")
+}
