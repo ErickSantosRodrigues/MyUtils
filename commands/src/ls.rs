7505ca2c1756b573f54f649e5272abb4a305c34d
ErickSantosRodrigues <esrerickssantos@gmail.com>
2023-12-21 12:24:39 -0300

    added colors to ls

--- commands/src/ls.rs
+++ commands/src/ls.rs
@@ -1,17 +1,20 @@
-// create my own ls command
-// usage: myls [path]
-// default path is current directory
-
-//use std::env;
 use std::fs;
-
+use colors::{Colors, to_color};
 pub fn ls(args: Vec<String>) {
         let paths = match args.len() {
             1 => fs::read_dir(".").unwrap(),
             _ => fs::read_dir(&args[1]).unwrap()
         };
         for path in paths {
-            println!("{}", path.unwrap().path().display());
+            let mut s = path.as_ref().unwrap().path().display().to_string();
+            s.remove(0);
+            s.remove(0);
+            if path.as_ref().unwrap().path().is_dir() {
+                s.push_str("/");
+                println!("{}", to_color(&s, Colors::Blue));
+            } else {
+                println!("{}", to_color(&s, Colors::Green)); 
+            }
         }
 }
 
